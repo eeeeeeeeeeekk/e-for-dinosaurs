18b32b2a0959167f5070058896b9eeee6fd4046d
Zeyu-Li <zeyu.li23@gmail.com>
2021-05-16 14:25:04 -0600

    removed unnecessary bits

--- cpp.cpp
+++ cpp.cpp
@@ -3,7 +3,6 @@
 #include <stdlib.h>
 #include <time.h>
 
-
 int main() {
     long long iterations;
     time_t t;
@@ -11,20 +10,17 @@ int main() {
     printf("Number of iterations: ");
     scanf("%llu", &iterations);
 
-    /* initializes pseudo random number generator */
     srand((unsigned) time(&t));
 
     int sum_total;
     float temp;
     sum_total = 0;
 
-    /* for loop manual */ 
     long long i = 0;
     while (i < iterations) {
         temp = 0;
         
         while (temp < 1) {
-            /* get random float */
             temp += ((float)rand()/(float)(RAND_MAX)); 
             
             sum_total++;
@@ -32,7 +28,6 @@ int main() {
         i++;
     }
 
-    /* printf("%llu %llu\n", sum_total, iterations); */
     printf("e = %lf\n", ((double)sum_total) / iterations);
 
     return(0);
