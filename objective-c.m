ae35e2ea886f522838f67ee336bd8ffc064b0e34
Zeyu-Li <zeyu.li23@gmail.com>
2021-05-16 19:05:53 -0600

    copied objective-c

---
+++ objective-c.m
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main() {
+    long long iterations;
+    time_t t;
+
+    printf("Number of iterations: ");
+    scanf("%llu", &iterations);
+
+    /* initializes pseudo random number generator */
+    srand((unsigned) time(&t));
+
+    int sum_total;
+    float temp;
+    sum_total = 0;
+
+    long long i = 0;
+    while (i < iterations) {
+        temp = 0;
+        
+        while (temp < 1) {
+            temp += ((float)rand()/(float)(RAND_MAX)); 
+            
+            sum_total++;
+        }
+        i++;
+    }
+
+    printf("e = %lf\n", ((double)sum_total) / iterations);
+
+    return(0);
+}
